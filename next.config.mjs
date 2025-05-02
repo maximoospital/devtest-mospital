849348b8b47b0793eac64ffa1c48086e21dc6f53
Maximo Ospital <59743027+maximoospital@users.noreply.github.com>
2025-05-02 13:29:56 -0300

    testing gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const basePath = '/devtest-mospital';
-const assetPrefix = `${basePath}/`;
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/devtest-mospital' : '';
+const assetPrefix = isProd ? '/devtest-mospital/' : '';
 
 const nextConfig = {
   basePath,
